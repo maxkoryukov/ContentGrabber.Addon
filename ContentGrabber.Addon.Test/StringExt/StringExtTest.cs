d20156fd166e5e4e4f5b5dda7622b26af97d2c07
maxkoryukov <maxkoryukov@yandex.ru>
2017-01-14 23:56:11 +0500

    Coverage

--- ContentGrabber.Addon.Test/StringExt/StringExtTest.cs
+++ ContentGrabber.Addon.Test/StringExt/StringExtTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
+using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace ContentGrabber.Addon.Test.StringExtTest {
 
@@ -76,6 +78,50 @@ namespace ContentGrabber.Addon.Test.StringExtTest {
 		[TestCase("a two four five", new string [] { "ten" }, ExpectedResult=false)]
 		public bool Normal(string str, string [] subs) {
 			return str.ContainsAny(subs);
+		}
+	}
+
+	[TestFixture]
+	public class MatchFirstNotEmpty {
+		[Test]
+		[TestCase("a two four five", new string [] { @"\d", @"\w{3}" }, ExpectedResult="two")]
+		[TestCase("a two four five", new string [] { @"[+%<>]", @"\w+" }, ExpectedResult="a")]
+		[TestCase("a two four five", new string [] { @"\d" }, ExpectedResult=null)]
+		public string MatchFirstNotEmpty_Strings(string str, string [] string_patterns) {
+			return str.MatchFirstNotEmpty(string_patterns);
+		}
+
+		public static IEnumerable TestCases_MatchFirstNotEmpty_Regex() {
+			yield return new TestCaseData("a two four five", new Regex [] { new Regex(@"\d"), new Regex(@"\w{3}") }).Returns("two");
+			yield return new TestCaseData("a two four five", new Regex [] { new Regex(@"[+%<>]"), new Regex(@"\w+") }).Returns("a");
+			yield return new TestCaseData("a two four five", new Regex [] { new Regex(@"\d") }).Returns(null);
+		}
+
+		[Test]
+		[TestCaseSource("TestCases_MatchFirstNotEmpty_Regex")]
+		public string MatchFirstNotEmpty_Regex(string str, Regex [] regex_patterns) {
+			return str.MatchFirstNotEmpty(regex_patterns);
+		}
+	}
+
+	[TestFixture]
+	public class MatchNotEmpty {
+		[Test]
+		[TestCase("a two four five", new string [] { @"\w{3}" }, ExpectedResult=new string[] {"two", "fou", "fiv"})]
+		[TestCase("a two four five", new string [] { @"\d" }, ExpectedResult=new string[0])]
+		public string[] MatchNotEmpty_Strings(string str, string [] string_patterns) {
+			return str.MatchNotEmpty(string_patterns);
+		}
+
+		public static IEnumerable TestCases_MatchNotEmpty_Regex() {
+			yield return new TestCaseData("a two four five", new Regex [] { new Regex(@"\w{3}") }).Returns(new string[] {"two", "fou", "fiv"});
+			yield return new TestCaseData("a two four five", new Regex [] { new Regex(@"\d") }).Returns(new string[0]);
+		}
+
+		[Test]
+		[TestCaseSource("TestCases_MatchNotEmpty_Regex")]
+		public string[] MatchNotEmpty_Regex(string str, Regex [] regex_patterns) {
+			return str.MatchNotEmpty(regex_patterns);
 		}
 	}
 }
